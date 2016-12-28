2ddeb502d3d99bab2f6e8a4b4ab5c9fb4f171fee
Prashant Joshi <prashantjoshi221995@gmail.com>
2016-12-28 15:14:12 +0530

    Update Kafka_producer.java

--- Fetching_Real_Time_Data /src/main/java/Kafka_producer.java
+++ Fetching_Real_Time_Data /src/main/java/Kafka_producer.java
@@ -6,13 +6,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Properties;
-
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
-
-
-
 
 public class Kafka_producer {
 
@@ -23,38 +19,27 @@ public class Kafka_producer {
 		String line =null;
 		
 		Properties props = new Properties();
-		
 		props.put("zk.connect","localhost:2181");
 		props.put("serializer.class","kafka.serializer.StringEncoder");
 		props.put("metadata.broker.list","localhost:9092");
 		props.put("request.required.acks","1");
-		
 		ProducerConfig producer_config = new ProducerConfig(props);
-		
 		Producer p = new Producer(producer_config);
-		
 		File f =new File("/home/prashant/Desktop/f1");
-			
-		while(true)  
+			while(true)  
 			{
-			
 			RandomAccessFile raf = null;
-				if((presentLength = f.length()) > lengthBefore)
-					{
+				if((presentLength = f.length()) > lengthBefore){
 				raf	=new  RandomAccessFile(f,"r"); //here "r" argument stands for Read operation .
 					raf.seek(lengthBefore);        //seek will set the pointer to that position from where next read will occur.
 					lengthBefore =presentLength;
-					while((line =raf.readLine()) != null)
-						{
-						
-						p.send(new KeyedMessage("Reading_logs",line));	// This will send the data to Kafka Broker.
+					while((line =raf.readLine()) != null){
+					p.send(new KeyedMessage("Reading_logs",line));	// This will send the data to Kafka Broker.
 						}
 					
 					}
-				
-				
-			}
-		
+						
+			}	
 	}
 	
 }
